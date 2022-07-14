87ee061fa2d41fd7c1ae55144dfdff4741dae6a8
gioleon <giovannileon2017@gmail.com>
2022-07-14 16:47:09 -0500

    adding of a frame

--- home.py
+++ home.py
@@ -6,14 +6,10 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("../data/diabetes.csv")
-
-df['Insulin'] = df['Insulin'].replace(0, df['Insulin'].median)
-
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],  meta_tags=[
            {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
 
-app.title = "Diabetes CSV"
+app.title = "por definir"
 
 app.layout = dbc.Container(
     [html.H1(["Diabetes Features distributions"]),
@@ -22,12 +18,7 @@ app.layout = dbc.Container(
          children=[
              dbc.Col(
                  [
-                     dbc.Label("Please select one feature"),
-                     dcc.Dropdown(
-                         id="variable",
-                         options=[{"label": i, "value": i}
-                                  for i in df.columns],
-                         value="Pregnancies",
+
                      )
                  ]),
 
@@ -44,15 +35,7 @@ app.layout = dbc.Container(
     Input(component_id="variable", component_property="value")
 )
 def make_graph(feature):
-    if feature != "Outcome":
-        fig = px.histogram(df, x=feature, opacity=0.8,
-                           color_discrete_sequence=['indianred'])
-    else:
-        fig = px.bar(df[feature].value_counts())
-        fig.update_xaxes(tickvals=[0, 1], ticktext=["Positive", "Negative"])
-        fig.update_traces(marker_color='indianred', opacity=0.8)
-
-    return fig
+    pass
 
 
 if __name__ == "__main__":
