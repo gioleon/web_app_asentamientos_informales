ed70a3c4a2df5477ece4efb361bf5f45800ea5fc
gioleon <giovannileon2017@gmail.com>
2022-07-16 15:50:06 -0500

    Preparation of the first plot (servicios)

--- home.py
+++ home.py
@@ -1,10 +1,12 @@
 # Importing libraries
-from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
-
-import dash_bootstrap_components as dbc
-
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
+import dash_bootstrap_components as dbc
+
+from prepare_data import graph_servicios_facturados
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],  meta_tags=[
            {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
@@ -18,15 +20,14 @@ app.layout = dbc.Container(
          children=[
              dbc.Col(
                  [
+                    
 
-                     )
                  ]),
 
              dcc.Graph(id="my-graph")
          ]
     )
     ]
-
 )
 
 
@@ -34,8 +35,22 @@ app.layout = dbc.Container(
     Output(component_id="my-graph", component_property="figure"),
     Input(component_id="variable", component_property="value")
 )
-def make_graph(feature):
-    pass
+def servicios_facturas():
+    df = graph_servicios_facturados()
+
+    fig = make_subplots(rows = 1, cols = 2)
+    fig.add_trace(
+        go.Pie(
+            labels = df.sum(axis = 0)/df.shape[0].index,
+            values = df.sum(axis = 0)/df.shape[0].values
+        ),
+        row = 1, col = 1
+    )
+
+    return fig
+
+
+
 
 
 if __name__ == "__main__":
--- prepare_data.py
+++ prepare_data.py
@@ -1,18 +1,116 @@
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def load_data() -> pd.DataFrame:
     """
     This function read the csv file
-    and returns a dataframe object
+    and returns a dataframe object only
+    wuith the columns of interest
 
-    @return: pandas dataframe object
+    @return: pandas dataframe object.
     """
+    cols = [
+        "nombre del sector a caracterizar",
+        "codigo de la vivienda",
+        "cuantos pisos tiene la vivienda?",
+        "tipo de vivienda",
+        "¿durante la noche # espacios de la vivienda"
+        + " se emplean como dormitorios",
+        "estado general de la vivienda",
+        "de que servicios públicos recibe factura?",
+        'el servicio de acueducto llega a su vivienda las 24 horas al dia',
+        'el servicio de energia llega a su vivienda las 24 horas al dia d',
+        "responsable del hogar: genero",
+        "responsable del hogar: ¿tiene diploma de alguno de los "
+        + "siguiente niveles de escolaridad?",
+        'cuantos hogares habitan esta vivienda?'
+    ]
+
     df = pd.read_csv(
+
         "data/Censo_de_asentamientos_informales_Cartagena_2021.csv",
         encoding = "UTF-8"
     )
 
+    df = df.rename(lambda x : str(x).lower(), axis = 1)
+    df = df[cols].drop_duplicates()
+
+    return  df
+
+
+def rename_cols(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function renames the columns
+    of the dataframe.
+    """
+    cols_to_rename = [
+        "sector",
+        "codigo_vivienda",
+        "n_pisos",
+        "tipo_vivienda",
+        "n_espacio_dormitorios_noche",
+        "general_vivienda",
+        "servicios_facturas",
+        "acueducto_24/7",
+        'energia_24/7',
+        "genero_responsable",
+        "diplomas_responsable",
+        'n_hogares_vivienda'
+    ]
+
+    df.columns = cols_to_rename
+
     return df
 
-print(load_data().head(5))
+
+df = rename_cols(load_data())
+df.shape
+
+
+def graph_servicios_facturados(sector = "all"):
+    """
+    This function will return a graphic of the
+    bill services that each house receive.
+    """
+    if sector == "all":
+        servicios_facturas = df.servicios_facturas.apply(str.lower)
+    else:
+        servicios_facturas = df["servicios_facturas"][
+            df["sector"] == sector
+        ].apply(str.lower)
+
+    index_cols = servicios_facturas.apply(
+        lambda x: len(x.split())
+    ).sort_values().index[-1]
+
+    cols = servicios_facturas.loc[index_cols].split(maxsplit = 5)
+
+    df_servicios = pd.DataFrame(
+        columns = cols + ["ninguno"],
+        index = servicios_facturas.index
+    )
+
+    dict_temp = {}
+    for i in range(servicios_facturas.shape[0]):
+        dict_temp[df.codigo_vivienda.iloc[i]] = np.array(
+            pd.Series(cols+["ninguno"]).isin(
+                servicios_facturas.iloc[i].split(maxsplit=5)
+            )
+        )*1
+
+    services_df = pd.DataFrame.from_dict(
+        dict_temp,
+        orient = "index",
+        columns = cols + ["ninguno"]
+    )
+
+    return services_df
+
+
+graph_servicios_facturados().sum(axis = 0)
